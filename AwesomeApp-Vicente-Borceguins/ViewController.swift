d3c1bcaabfbadc69b48ac7859cbe7a2bb178a834
Vicente Borceguin <vborceguin@mail.sfsu.edu>
2020-10-21 21:54:13 -0700

    Initial Commit

---
+++ AwesomeApp-Vicente-Borceguins/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AwesomeApp-Vicente-Borceguins
+//
+//  Created by Vince Borceguin on 10/21/20.
+//  Copyright © 2020 SFSU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
